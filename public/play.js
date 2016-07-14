4c490dd29cb871cc6bd39da4f2b379b7b6920d6c
Saud Ashanike <Saud.Ashanike@softwire.com>
2016-07-14 13:35:23 +0100

    Fetches URL play page

--- public/play.js
+++ public/play.js
@@ -10,3 +10,45 @@ firebase.database().ref("users/" + op.name).on('value', function(data) {
     var username = data.val() ? data.val().Name : "guest";
     $("#usr").html(cap(username));
 });
+
+function showGameData () {
+    firebase.database().ref('games/'+fetchID()+).once("value",
+        function(data){
+            var DOC = data.val().DOC;
+            var boardState = {
+                a: {
+                    1: {
+                        covered: false,
+                        role: null,
+                        word: null
+                    },
+                    2: {
+                        covered: false,
+                        role: null,
+                        word: null
+                    }
+                },
+                b: {
+                    1: {
+                        covered: false,
+                        role: null,
+                        word: null
+                    }
+                    2: {
+                        covered: false,
+                        role: null,
+                        word: null
+                    }
+                }
+            }
+
+            var redTeam = data.val().players
+            var blueTeam =
+            console.log(boardState,DOC,redTeam,blueTeam);
+        }
+    )
+}
+
+function fetchID(){
+    return window.location.href.substr(window.location.href.indexOf("?g=")+3);
+}
