eaf56b7aa410d2eb9cf3d43a0304bb100308bfe4
TOA <tobi.akiny@gmail.com>
2016-07-21 11:41:54 +0100

    Refactored turning function, to execute on the event of a board square change

--- public/resources/javascript/play.js
+++ public/resources/javascript/play.js
@@ -1,7 +1,7 @@
 define(['jquery', 'firebase'], function($, firebase) {
     var game = decodeURIComponent((new RegExp('[?|&]g=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null,
         spectate = decodeURIComponent((new RegExp('[?|&]spectate=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null,
-        boardSqaures = ["a1","a2","b1","b2"];
+        boardSqaures = ["a1", "a2", "b1", "b2"];
     // grabbing URL Parameters;
 
     function fill(board) {
@@ -11,36 +11,36 @@ define(['jquery', 'firebase'], function($, firebase) {
         // Loops through every square and fills word;
     }
 
-    firebase.database().ref('games/' + game).on("value", function(game) {
+    firebase.database().ref('games/' + game).on("value", function(gameD) {
         if (!$("#redTeam").html().replace(/\s/g, "") && !$("#blueTeam").html().replace(/\s/g, "")) { // checks if filled
-            for (player in game.val().players) { // Loops through the database to find members of the red team
-                if (game.val().players[player].team == "red") {
+            for (player in gameD.val().players) { // Loops through the database to find members of the red team
+                if (gameD.val().players[player].team == "red") {
                     $("#redTeam").append("<span>" + player + "</span><br/>")
-                } else if (game.val().players[player].team == "blue") {
+                } else if (gameD.val().players[player].team == "blue") {
                     $("#blueTeam").append("<span>" + player + "</span><br/>")
                 }
             }
             // fills teams;
         }
-        fill(game.val().board);
+        fill(gameD.val().board);
 
         // TODO: RECKTIFY THIS NESTING;
 
-        if (player = game.val().players[op.name]) {
+        if (player = gameD.val().players[op.name]) {
             if (player.perm == "master") {
                 var view = document.createElement("div"),
                     squares = [];
                 for (var i = 0; i < 4; ++i) {
-                    squares[i]=document.createElement("div");
-                    squares[i].id = "v"+boardSqaures[i];
-                    $(squares[i]).addClass(game.val().board[boardSqaures[i]].role);
+                    squares[i] = document.createElement("div");
+                    squares[i].id = "v" + boardSqaures[i];
+                    $(squares[i]).addClass(gameD.val().board[boardSqaures[i]].role);
                 }
-                squares.filter(function(square){
+                squares.filter(function(square) {
                     $(view).append(square);
                 })
                 view.id = "masterv";
                 $("body").append(view);
-                $("#board").css("bottom","300px");
+                $("#board").css("bottom", "300px");
             } else {
                 $(".word").click(function() {
                     var word = this;
@@ -51,13 +51,14 @@ define(['jquery', 'firebase'], function($, firebase) {
                             game: game,
                             square: word.parentElement.id,
                             name: op.name
-                        },
-                        success: function(a, b, c) {
-                            $("#" + word.parentElement.id + " > .role").html(c.responseText).css("transform", "rotateY(360deg)")
-                            console.log(a, b, c);
-                            $(word).css("transform", "rotateY(180deg)");
                         }
                     });
+                })
+                firebase.database().ref("games/"+game+"/board").on("child_changed",function(square){
+                    if (square.val().covered) {
+                        $("#" + square.key + " > .role").html(square.val().role).css("transform", "rotateY(360deg)");
+                        $("#" + square.key + " > .word").css("transform", "rotateY(180deg)");
+                    }
                 })
                 // Fills Role, and then flips, when corresponding word card is clicked;
             }
