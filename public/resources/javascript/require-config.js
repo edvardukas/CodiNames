71efca7be762c26891f0070fb5f8c3dcd43753d0
TOA <tobi.akiny@gmail.com>
2016-07-21 16:22:21 +0100

    renamed require configuration for Sign in

--- public/resources/javascript/require-config.js
+++ public/resources/javascript/require-config.js
@@ -5,7 +5,7 @@ requirejs.config({
 		home: 'home',
 		header: "header",
 		play: "play",
-		sign-in: "sign-in",
+		signin: "sign-in",
 
 		firebase_app  : 'https://www.gstatic.com/firebasejs/3.1.0/firebase-app',
 		firebase_auth : 'https://www.gstatic.com/firebasejs/3.1.0/firebase-auth',
