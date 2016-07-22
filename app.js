47fc0e10a944da79b71feb0f8dcc44c5eae01998
TOA <tobi.akiny@gmail.com>
2016-07-22 14:48:42 +0100

    game turn changing fixed

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express = require('express'),
     port = 3000,
     firebaseApp = firebase.initializeApp(firebaseConfig.database),
     bodyParser = require('body-parser');
-    app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'null');
@@ -18,21 +18,24 @@ app.use(function (req, res, next) {
 
 app.use(express.static('public'));
 
-app.post("/turn",function(req,res){
+app.post("/turn", function (req, res) {
     var nextTurn,
         player;
-    firebase.database().ref("games/"+req.body.game).once("value",function(game){
+    firebase.database().ref("games/" + req.body.game).once("value", function (game) {
         console.log(game.val().players[req.body.name]);
         player = game.val().players[req.body.name];
-        nextTurn = (game.val().turn == "red")? "blue":"red";
-        console.log(nextTurn);
+        nextTurn = (game.val().turn == "red") ? "blue" : "red";
+        console.log(req.body.square,nextTurn);
         if (player.team === game.val().turn) {
-            firebase.database().ref("games/"+req.body.game+"/turn").set(nextTurn).then(function(){
-                firebase.database().ref("games/"+req.body.game+"/board/"+req.body.square+"/covered").set(true);
-            })
+            firebase.database().ref("games/" + req.body.game + "/board/" + req.body.square + "/covered").set(true);
+            if (game.val().board[req.body.square].role == nextTurn || game.val().board[req.body.square].role == "bystander") {
+                firebase.database().ref("games/" + req.body.game + "/turn").set(nextTurn)
+            }
+
         } else res.send();
     })
+    res.send();
 })
-app.listen(port, function() {
-  console.log('Express listening on port ' + port);
+app.listen(port, function () {
+    console.log('Express listening on port ' + port);
 });
