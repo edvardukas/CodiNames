f9cec4e3ff9c24b85a1531ee4dc5a550cd3f33d4
Tobi <tobi.akiny@gmail.com>
2016-07-23 15:19:27 +0100

    app.js resolved

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 var express = require('express'),
     app = express(),
     firebase = require('firebase'),
-    cors = require('cors'),
     firebaseConfig = require('./firebase.json'),
     port = 3000,
     firebaseApp = firebase.initializeApp(firebaseConfig.database),
     bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'null');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static('public'));
 
